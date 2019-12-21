c06a4c6355037dc25d8bd8d5aebcaf7f065fe020
Elson Jose <elsonjoseofficial@gmail.com>
2019-12-21 15:12:42 +0530

    Ui for main done

--- app/src/main/java/com/simple/pinata/MainActivity.java
+++ app/src/main/java/com/simple/pinata/MainActivity.java
@@ -23,11 +23,16 @@ import android.view.animation.AnimationUtils;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ViewFlipper;
 
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.io.BufferedInputStream;
 import java.io.File;
@@ -50,12 +55,36 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    private Button AddBdayBtn,ViewBdayBtn;
+    private RelativeLayout Root;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Root = findViewById(R.id.main_root);
+        AddBdayBtn = findViewById(R.id.main_add_bday_btn);
+        ViewBdayBtn = findViewById(R.id.main_view_bday_btn);
 
+        AddBdayBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Snackbar.make(Root,"Go online for seamless device accesses.",Snackbar.LENGTH_SHORT).show();
+
+            }
+        });
+
+        ViewBdayBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Snackbar.make(Root,"Go online for seamless device accesses.",Snackbar.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 
